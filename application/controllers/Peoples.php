67486cf014276034a8b86af3fd6920d6c6281906
dp07 <dp.madridiztazc@gmail.com>
2019-12-26 21:51:46 +0700

    update

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -6,43 +6,24 @@
             $data['judul'] = 'Halaman Peoples';
             
             $this->load->model('Peoples_model', 'people');
-            
-            // styling
-            $config['full_tag_open'] = '<nav aria-label="Page navigation example"><ul class="pagination justify-content-center">';
-            $config['full_tag_close'] = '</ul></nav>';
-            
-            $config['first_link'] = 'First';
-            $config['first_tag_open'] = '<li class="page-item">';
-            $config['first_tag_close'] = '</li>';
-
-            $config['last_link'] = 'Last';
-            $config['last_tag_open'] = '<li class="page-item">';
-            $config['last_tag_close'] = '</li>';
-
-            $config['next_link'] = '&raquo';
-            $config['next_tag_open'] = '<li class="page-item">';
-            $config['next_tag_close'] = '</li>';
-
-            $config['prev_link'] = '&laquo';
-            $config['prev_tag_open'] = '<li class="page-item">';
-            $config['prev_tag_close'] = '</li>';
-
-            $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
-            $config['cur_tag_close'] = '</a></li>';
-
-            $config['num_tag_open'] = '<li class="page-item">';
-            $config['num_tag_close'] = '</li>';
-
-            $config['attributes'] = array('class' => 'page-link');
 
             // load library
             $this->load->library('pagination');
 
+            if($this->input->post('keyword'))
+            {
+                $data['keyword'] = $this->input->post('keyword');
+            } 
+            else
+            {
+                $data['keyword'] = null;
+            }
+
             // config
-            $config['base_url'] = 'http://localhost/ci-app/peoples/index';
             $config['total_rows'] = $this->people->countAllRows();
-            $config['per_page'] = 9;
-            $config['num_links'] = 5;
+            $data['total_row'] = $config['total_rows'];
+            $config['per_page'] = 5;
+            
 
             // initilize
             $this->pagination->initialize($config);
@@ -50,7 +31,7 @@
             // ambil data page dari url
             $data['start'] = $this->uri->segment(3);
 
-            $data['peoples'] = $this->people->getPeoples($config['per_page'], $data['start']);
+            $data['peoples'] = $this->people->getPeoples($config['per_page'], $data['start'], $data['keyword']);
             $this->load->view('templates/header', $data);
             $this->load->view('peoples/index', $data);
             $this->load->view('templates/footer');
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -2,7 +2,15 @@
     <div class="row">
         <div class="col-md-10 mt-5">
         <h3>List Of Peoples</h3>
-        <?php  ?>
+        <form action="" method="post">
+        <div class="input-group mb-3 col-md-6 float-right">
+            <input type="text" class="form-control" placeholder="Search Keyword..." name="keyword" autocomplete="off" autofocus>
+            <div class="input-group-append">
+                <button class="btn btn-success" type="submit" name="submit">Button</button>
+            </div>
+        </div>
+        </form>
+        <h5 class="mt-5">Results: <?= $total_row;  ?> </h5>
         <table class="table">
             <thead>
                 <tr>
@@ -13,6 +21,15 @@
                 </tr>
             </thead>
             <tbody>
+            <tr>
+                <td colspan="4">
+                <?php if(empty($peoples)) : ?>
+                    <div class="alert alert-danger" role="alert">
+                    data not found!!
+                    </div>
+                <?php endif; ?>
+                </td>
+            </tr>
            
             <?php foreach($peoples as $people) : ?>
                 <tr>
